39bb6ada04eda9707dcb7bd53afd1944dfa2e9d3
hanzyn09 <ysurkkr12@naver.com>
2025-01-28 18:51:49 +0900

    refactor code

--- tamagotchi/src/main/java/tamagotchi/service/TamagotchiServiceImpl.java
+++ tamagotchi/src/main/java/tamagotchi/service/TamagotchiServiceImpl.java
@@ -17,206 +17,201 @@ import tamagotchi.mapper.TamagotchiMapper;
 @Slf4j
 @Service
 public class TamagotchiServiceImpl implements TamagotchiService {
-	@Autowired
-	private TamagotchiMapper tamagochiMapper;
+    @Autowired
+    private TamagotchiMapper tamagochiMapper;
 
-	@Autowired
-	private FileUtils fileUtils;
+    @Autowired
+    private FileUtils fileUtils;
 
-	@Override
-	public List<TamagotchiDto> selectTamagotchiList() {
-		return tamagochiMapper.selectTamagotchiList();
-	}
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 100;
 
-	@Scheduled(fixedRate = 30000) // 30초마다 실행 (30000ms)
+    @Override
+    public List<TamagotchiDto> selectTamagotchiList() {
+        return tamagochiMapper.selectTamagotchiList();
+    }
+
+    @Scheduled(fixedRate = 30000) // 30초마다 실행
     public void autoUpdateState() {
         log.info("자동 타마고치 상태 업데이트 시작");
         List<TamagotchiDto> tamagotchiList = selectTamagotchiList();
-        if (tamagotchiList != null && !tamagotchiList.isEmpty()) {
-        	this.updateDate("day");
+        if (!CollectionUtils.isEmpty(tamagotchiList)) {
+            updateDate("day");
         }
         log.info("자동 타마고치 상태 업데이트 완료");
     }
-	
-	@Override
-	public void createTamagotchi(String name, MultipartHttpServletRequest request) {
-		// 1. 다마고치 등록
-		TamagotchiDto tamagotchiDto = new TamagotchiDto();
-		tamagotchiDto.setName(name);
-		tamagotchiDto.setLevelNumber(1);
-		tamagotchiDto.setHunger(50);
-		tamagotchiDto.setFatigue(50);
-		tamagotchiDto.setHappiness(50);
-		tamagochiMapper.insertTamagotchi(tamagotchiDto);
 
-		// 2. 이미지 업로드 처리
-		try {
-			// 첨부 파일을 디스크에 저장하고, 첨부 파일 정보를 반환
-			List<TamagotchiFileDto> fileInfoList = fileUtils.parseFileInfo(tamagotchiDto.getTamagotchiId(), request);
-			// 첨부 파일 정보를 DB에 저장 (이미지가 있을 경우)
-			if (!CollectionUtils.isEmpty(fileInfoList)) {
-				tamagochiMapper.insertTamagotchiFileList(fileInfoList);
-			}
-		} catch (Exception e) {
-			log.error(e.getMessage());
-		}
-	}
+    @Override
+    public void createTamagotchi(String name, MultipartHttpServletRequest request) {
+        TamagotchiDto tamagotchiDto = new TamagotchiDto();
+        tamagotchiDto.setName(name);
+        tamagotchiDto.setLevelNumber(1);
+        tamagotchiDto.setHunger(50);
+        tamagotchiDto.setFatigue(50);
+        tamagotchiDto.setHappiness(50);
+        tamagochiMapper.insertTamagotchi(tamagotchiDto);
 
-	@Override
-	public TamagotchiDto selectTamagotchiDetail(int tamagotchiId) {
-		TamagotchiDto tamagotchiDto = tamagochiMapper.selectTamagotchiDetail(tamagotchiId);
-		List<TamagotchiFileDto> tamagotchiFileInfoList = tamagochiMapper.selectTamagotchiFileList(tamagotchiId);
-		tamagotchiDto.setFileInfoList(tamagotchiFileInfoList);
+        try {
+            List<TamagotchiFileDto> fileInfoList = fileUtils.parseFileInfo(tamagotchiDto.getTamagotchiId(), request);
+            if (!CollectionUtils.isEmpty(fileInfoList)) {
+                tamagochiMapper.insertTamagotchiFileList(fileInfoList);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
 
-		return tamagotchiDto;
-	}
+    @Override
+    public TamagotchiDto selectTamagotchiDetail(int tamagotchiId) {
+        TamagotchiDto tamagotchiDto = tamagochiMapper.selectTamagotchiDetail(tamagotchiId);
+        List<TamagotchiFileDto> fileInfoList = tamagochiMapper.selectTamagotchiFileList(tamagotchiId);
+        tamagotchiDto.setFileInfoList(fileInfoList);
+        return tamagotchiDto;
+    }
 
-	@Override
-	public String updateState(int tamagotchiId, String action) {
-		TamagotchiDto tamagotchiDto = tamagochiMapper.selectTamagotchiDetail(tamagotchiId);
-		TamagotchiDto tamagotchiDtoTmp = new TamagotchiDto();
+    @Override
+    public String updateState(int tamagotchiId, String action) {
+        TamagotchiDto tamagotchiDto = tamagochiMapper.selectTamagotchiDetail(tamagotchiId);
+        TamagotchiDto tamagotchiDtoTmp = new TamagotchiDto();
+        tamagotchiDtoTmp.setTamagotchiId(tamagotchiId);
 
-		String message = tamagotchiDto.getName();
-		
-		// 기존값 셋팅
-		int oldLevelNumber = tamagotchiDto.getLevelNumber();
-		int oldHunger = tamagotchiDto.getHunger();
-		int oldFatigue = tamagotchiDto.getFatigue();
-		int oldHappiness = tamagotchiDto.getHappiness();
+        String message = "";
 
-		tamagotchiDtoTmp.setTamagotchiId(tamagotchiId);
+        // 상태 업데이트 메서드 호출
+        message = updateTamagotchiState(action, tamagotchiDto, tamagotchiDtoTmp);
 
-		// 액션에 따른 상태 업데이트
-		 message = updateTamagotchiState(action, message, oldLevelNumber, oldHunger, oldFatigue, oldHappiness, tamagotchiDtoTmp);
+        tamagochiMapper.updateState(tamagotchiDtoTmp);
+        return message;
+    }
 
-		// 업데이트된 값을 DB에 반영
-		tamagochiMapper.updateState(tamagotchiDtoTmp);
-		
-		return message;
-	}
+    private String updateTamagotchiState(String action, TamagotchiDto tamagotchiDto, TamagotchiDto tamagotchiDtoTmp) {
+        int oldLevelNumber = tamagotchiDto.getLevelNumber();
+        int oldHunger = tamagotchiDto.getHunger();
+        int oldFatigue = tamagotchiDto.getFatigue();
+        int oldHappiness = tamagotchiDto.getHappiness();
+        String message = tamagotchiDto.getName();
 
-	private String updateTamagotchiState(String action, String message, int oldLevelNumber, int oldHunger, int oldFatigue,
-										int oldHappiness, TamagotchiDto tamagotchiDtoTmp) {
-		final int minValue = 0;
-		final int maxValue = 100;
-		
-		switch (action) {
-		case "hunger":
-			int newHunger = Math.min(Math.max(oldHunger - 5, minValue), maxValue);
-			
-			message += "(은)는 먹이를 먹었습니다.";
-			
-			if (newHunger == 0 && oldFatigue == 0 && oldHappiness == 100) {
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
-				newHunger = 50;
-				oldFatigue = 50;
-				oldHappiness = 50;
-				
-				message += " 그리고 진화했습니다!";
-			} else 
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber);
-			
-			tamagotchiDtoTmp.setHunger(newHunger);
-			tamagotchiDtoTmp.setFatigue(oldFatigue);
-			tamagotchiDtoTmp.setHappiness(oldHappiness);
-			
-			break;
+        switch (action) {
+            case "hunger":
+                message += "(은)는 먹이를 먹었습니다.";
+                message = updateHunger(tamagotchiDtoTmp, oldLevelNumber, oldHunger, oldFatigue, oldHappiness, message);
+                break;
+            case "sleep":
+                message += "(은)는 푹 잤습니다.";
+                message = updateSleep(tamagotchiDtoTmp, oldLevelNumber, oldHunger, oldFatigue, oldHappiness, message);
+                break;
+            case "play":
+                message += "(은)는 신나게 놀았습니다.";
+                message = updatePlay(tamagotchiDtoTmp, oldLevelNumber, oldHunger, oldFatigue, oldHappiness, message);
+                break;
+            case "day":
+                //message += "하루가 경과했습니다.";
+                message = updateDay(tamagotchiDtoTmp, oldLevelNumber, oldHunger, oldFatigue, oldHappiness, message);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid action: " + action);
+        }
 
-		case "sleep":
-			int newFatigue = Math.min(Math.max(oldFatigue - 5, minValue), maxValue);
-			
-			message += "(은)는 푹 잤습니다.";
-			
-			if (oldHunger == 0 && newFatigue == 0 && oldHappiness == 100) {
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
-				oldHunger = 50;
-				newFatigue = 50;
-				oldHappiness = 50;
-				
-				message += " 그리고 진화했습니다!";
-			} else 
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber);
-			
-			tamagotchiDtoTmp.setHunger(oldHunger);
-			tamagotchiDtoTmp.setFatigue(newFatigue);
-			tamagotchiDtoTmp.setHappiness(oldHappiness);
-			
-			break;
+        return message;
+    }
 
-		case "play":
-			int newPlayFatigue = Math.min(Math.max(oldFatigue + 5, minValue), maxValue);
-			int newPlayHappiness = Math.min(Math.max(oldHappiness + 5, minValue), maxValue);
-			
-			message += "(은)는 신나게 놀았습니다.";
-			
-			if (oldHunger == 0 && newPlayFatigue == 0 && newPlayHappiness == 100) {
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
-				oldHunger = 50;
-				newPlayFatigue = 50;
-				newPlayHappiness = 50;
-				
-				message += " 그리고 진화했습니다!";
-			} else 
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber);
-			
-			tamagotchiDtoTmp.setHunger(oldHunger);
-			tamagotchiDtoTmp.setFatigue(newPlayFatigue);
-			tamagotchiDtoTmp.setHappiness(newPlayHappiness);
-			
-			break;
-			
-		case "day":
-			int newDayHunger = Math.min(Math.max(oldHunger + 10, minValue), maxValue);
-			int newDayFatigue = Math.min(Math.max(oldFatigue + 10, minValue), maxValue);
-			int newDayHappiness = Math.min(Math.max(oldHappiness - 10, minValue), maxValue);
+    private String updateHunger(TamagotchiDto tamagotchiDtoTmp, int oldLevelNumber, int oldHunger, int oldFatigue, int oldHappiness, String message) {
+        int newHunger = Math.min(Math.max(oldHunger - 5, MIN_VALUE), MAX_VALUE);
+        tamagotchiDtoTmp.setHunger(newHunger);
+        tamagotchiDtoTmp.setFatigue(oldFatigue);
+        tamagotchiDtoTmp.setHappiness(oldHappiness);
+        tamagotchiDtoTmp.setLevelNumber(oldLevelNumber); // 레벨 유지
 
-			if (newDayHunger == 0 && newDayFatigue == 0 && newDayHappiness == 100) {
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
-				newDayHunger = 50;
-				newDayFatigue = 50;
-				newDayHappiness = 50;
-			} else
-				tamagotchiDtoTmp.setLevelNumber(oldLevelNumber);
+        // 레벨업 조건
+        if (newHunger == 0 && oldFatigue == 0 && oldHappiness == 100) {
+            tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
+            tamagotchiDtoTmp.setFatigue(50);
+            tamagotchiDtoTmp.setHunger(50);
+            tamagotchiDtoTmp.setHappiness(50);
+            message += " 그리고 진화했습니다!";
+        }
+        return message;
+    }
 
-			tamagotchiDtoTmp.setHunger(newDayHunger);
-			tamagotchiDtoTmp.setFatigue(newDayFatigue);
-			tamagotchiDtoTmp.setHappiness(newDayHappiness);
-			break;
-		default:
-			throw new IllegalArgumentException("Invalid action: " + action);
-		}
-		return message;
-	}
+    private String updateSleep(TamagotchiDto tamagotchiDtoTmp, int oldLevelNumber, int oldHunger, int oldFatigue, int oldHappiness, String message) {
+        int newFatigue = Math.min(Math.max(oldFatigue - 5, MIN_VALUE), MAX_VALUE);
+        tamagotchiDtoTmp.setFatigue(newFatigue);
+        tamagotchiDtoTmp.setHunger(oldHunger);
+        tamagotchiDtoTmp.setHappiness(oldHappiness);
+        tamagotchiDtoTmp.setLevelNumber(oldLevelNumber); // 레벨 유지
 
-	@Override
-	public String deleteTamagotchi(int tamagotchiId) {
-		String message = "다마고치가 입양갔습니다.";
-		tamagochiMapper.deleteTamagotchi(tamagotchiId);
-		return message;
-	}
+        // 레벨업 조건
+        if (oldHunger == 0 && newFatigue == 0 && oldHappiness == 100) {
+            tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
+            tamagotchiDtoTmp.setFatigue(50);
+            tamagotchiDtoTmp.setHunger(50);
+            tamagotchiDtoTmp.setHappiness(50);
+            message += " 그리고 진화했습니다!";
+        }
+        return message;
+    }
 
-	@Override
-	public TamagotchiFileDto selectTamagotchiFileInfo(int imageId, int tamagotchiId) {
-		return tamagochiMapper.selectTamagotchiFileInfo(imageId, tamagotchiId);
-	}
+    private String updatePlay(TamagotchiDto tamagotchiDtoTmp, int oldLevelNumber, int oldHunger, int oldFatigue, int oldHappiness, String message) {
+        int newFatigue = Math.min(Math.max(oldFatigue + 5, MIN_VALUE), MAX_VALUE);
+        int newHappiness = Math.min(Math.max(oldHappiness + 5, MIN_VALUE), MAX_VALUE);
+        tamagotchiDtoTmp.setFatigue(newFatigue);
+        tamagotchiDtoTmp.setHappiness(newHappiness);
+        tamagotchiDtoTmp.setHunger(oldHunger);
+        tamagotchiDtoTmp.setLevelNumber(oldLevelNumber); // 레벨 유지
 
-	@Override
-	public String updateDate(String action) {
-		List<TamagotchiDto> list = this.selectTamagotchiList();
-		String message = "하루가 경과했습니다.";
+        // 레벨업 조건
+        if (oldHunger == 0 && newFatigue == 0 && newHappiness == 100) {
+            tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
+            tamagotchiDtoTmp.setFatigue(50);
+            tamagotchiDtoTmp.setHunger(50);
+            tamagotchiDtoTmp.setHappiness(50);
+            message += " 그리고 진화했습니다!";
+        }
+        return message;
+    }
 
-		for (TamagotchiDto tamagotchi : list) {
-			TamagotchiDto tamagotchiDto = this.selectTamagotchiDetail(tamagotchi.getTamagotchiId());
-			TamagotchiDto tamagotchiDtoTmp = new TamagotchiDto();
+    private String updateDay(TamagotchiDto tamagotchiDtoTmp, int oldLevelNumber, int oldHunger, int oldFatigue, int oldHappiness, String message) {
+        int newHunger = Math.min(Math.max(oldHunger + 10, MIN_VALUE), MAX_VALUE);
+        int newFatigue = Math.min(Math.max(oldFatigue + 10, MIN_VALUE), MAX_VALUE);
+        int newHappiness = Math.min(Math.max(oldHappiness - 10, MIN_VALUE), MAX_VALUE);
 
-			tamagotchiDtoTmp.setTamagotchiId(tamagotchiDto.getTamagotchiId());
-			
-			 updateTamagotchiState(action, message, tamagotchiDto.getLevelNumber(), tamagotchiDto.getHunger(),
-	                    tamagotchiDto.getFatigue(), tamagotchiDto.getHappiness(), tamagotchiDtoTmp);
-			 
-			// 업데이트된 값을 DB에 반영
-			tamagochiMapper.updateState(tamagotchiDtoTmp);
-		}
-		return message;
-	}
+        tamagotchiDtoTmp.setHunger(newHunger);
+        tamagotchiDtoTmp.setFatigue(newFatigue);
+        tamagotchiDtoTmp.setHappiness(newHappiness);
+        tamagotchiDtoTmp.setLevelNumber(oldLevelNumber); // 레벨 유지
+
+        // 레벨업 조건
+        if (newHunger == 0 && newFatigue == 0 && newHappiness == 100) {
+            tamagotchiDtoTmp.setLevelNumber(oldLevelNumber + 1);
+            tamagotchiDtoTmp.setFatigue(50);
+            tamagotchiDtoTmp.setHunger(50);
+            tamagotchiDtoTmp.setHappiness(50);
+            //message += " 그리고 진화했습니다!";
+        }
+        return message;
+    }
+
+    @Override
+    public String deleteTamagotchi(int tamagotchiId) {
+        tamagochiMapper.deleteTamagotchi(tamagotchiId);
+        return "다마고치가 입양갔습니다.";
+    }
+
+    @Override
+    public TamagotchiFileDto selectTamagotchiFileInfo(int imageId, int tamagotchiId) {
+        return tamagochiMapper.selectTamagotchiFileInfo(imageId, tamagotchiId);
+    }
+
+    @Override
+    public String updateDate(String action) {
+        List<TamagotchiDto> list = this.selectTamagotchiList();
+        String message = "하루가 경과했습니다.";
+        for (TamagotchiDto tamagotchi : list) {
+            TamagotchiDto tamagotchiDtoTmp = new TamagotchiDto();
+            tamagotchiDtoTmp.setTamagotchiId(tamagotchi.getTamagotchiId());
+            message = updateTamagotchiState(action, tamagotchi, tamagotchiDtoTmp);
+            tamagochiMapper.updateState(tamagotchiDtoTmp);
+        }
+        return message;
+    }
 }
